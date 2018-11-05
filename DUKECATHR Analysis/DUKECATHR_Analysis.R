10420a1099ce9449e3660feebbc28783a08745c5
Gauri Manoj Kamat <gk81@duke.edu>
2018-11-05 13:43:43 -0500

    Minor Changes

--- DUKECATHR Analysis/DUKECATHR_Analysis.R
+++ DUKECATHR Analysis/DUKECATHR_Analysis.R
@@ -3,11 +3,12 @@ library(survival)
 library(survminer)
 library(ggplot2)
 library(dplyr)
+library(DataExplorer)
 
 
 ## Read in Data
 
-dat = read_csv("dukecathr.csv")
+dat = read.csv("dukecathr.csv")
 attach(dat)
 
 cathdat = data.frame(age = AGE_G, gender = GENDER, race = RACE_G, year = YRCATH_G, 
@@ -46,7 +47,7 @@ attach(cathdat)
 ## Create Survival Object
 
 surv_obj = Surv(time = time, event = death)
-# surv_obj
+surv_obj
 
 
 ## Unstratified Kaplan-Meier Estimate
@@ -68,7 +69,7 @@ survdiff(surv_obj ~ gender, data = cathdat)
 #### Race
 kp.race = survfit(surv_obj ~ race, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.race, data = cathdat,pval = TRUE)
-
+survdiff(surv_obj ~ race, data = cathdat)
 
 
 ### History of diseases
@@ -76,34 +77,42 @@ ggsurvplot(kp.race, data = cathdat,pval = TRUE)
 #### Cerebrovascular
 kp.cereb = survfit(surv_obj ~ histcereb, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.cereb, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histcereb, data = cathdat)
 
 #### Congestive Heart Failure
 kp.chf = survfit(surv_obj ~ histchf, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.chf, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histchf, data = cathdat)
 
 #### COPD
 kp.copd = survfit(surv_obj ~ histcopd, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.copd, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histcopd, data = cathdat)
 
 #### Diabetes
 kp.diab = survfit(surv_obj ~ histdiab, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.diab, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histdiab, data = cathdat)
 
 #### Hypertension
 kp.hypt = survfit(surv_obj ~ histhyp, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.hypt, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histhyp, data = cathdat)
 
 #### Hyperlipidemia
 kp.hyplip = survfit(surv_obj ~ histhyplip, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.hyplip, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histhyplip, data = cathdat)
 
 #### Myocardial Infarction
 kp.myo = survfit(surv_obj ~ histmyo, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.myo, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histmyo, data = cathdat)
 
 #### Smoking
 kp.smoke = survfit(surv_obj ~ histsmoke, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.smoke, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ histsmoke, data = cathdat)
 
 
 
@@ -112,14 +121,19 @@ ggsurvplot(kp.smoke, data = cathdat,pval = TRUE)
 #### S3 Gallop
 kp.s3g = survfit(surv_obj ~ s3g, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.s3g, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ s3g, data = cathdat)
 
 
 #### No. of diseased vessels
 kp.disves = survfit(surv_obj ~ numdisves, data = cathdat,na.action = na.omit)
 ggsurvplot(kp.disves, data = cathdat,pval = TRUE)
+survdiff(surv_obj ~ numdisves, data = cathdat)
 
 
-# Note --> Not estimating KM curves for chfsev,maxsten(continuous)  and also year and age group (too many categories)
+# Note --> Not estimating KM curves for leftvenEF,chfsev,maxsten(continuous)  
+# To stratify KM curves by year and age group
 
+## MISSING COVARIATES
 
+plot_missing(cathdat)
 
